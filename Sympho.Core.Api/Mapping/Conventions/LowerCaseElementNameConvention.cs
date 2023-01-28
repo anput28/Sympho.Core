2848e89e23ff4f7e03bb2885eee927b3c31f63ce
anput28 <angelo.putignano.16@gmail.com>
2023-01-28 20:36:39 +0100

    Aggiornamento meccanismo di mapping delle entità

--- Sympho.Core.Api/Mapping/Conventions/LowerCaseElementNameConvention.cs
+++ Sympho.Core.Api/Mapping/Conventions/LowerCaseElementNameConvention.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Conventions;
 
 namespace Sympho.Core.Api.Mapping.Conventions {
     public class LowerCaseElementNameConvention : IMemberMapConvention {
-        public string Name => "LowerCaseElementName";
+        public string Name => nameof(LowerCaseElementNameConvention);
 
         public void Apply(BsonMemberMap memberMap) {
             memberMap.SetElementName(memberMap.ElementName.ToLower());
